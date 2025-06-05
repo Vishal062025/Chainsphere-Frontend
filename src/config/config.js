15f3fdbef5faf382313f942926416b53d6addab9
Vishal062025 <vishal@intelisync.ai>
2025-06-05 09:53:24 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,6 @@
 // export const BASE_URL1 = "https://chainsphere.tech/";
 
 
-export const BASE_URL = "http://13.204.13.64/backend/";
+export const BASE_URL = "https://chainsphere.tech/backend";
 export const BASE_URL_ = "http://127.0.0.1:5000";
 export const BASE_URL1 = "http://localhost:5000/";
