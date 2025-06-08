dfc5783353b1082c62de6c2a63ca12b9d57797e6
Vishal062025 <vishal@intelisync.ai>
2025-06-08 16:37:19 +0530

    added usdt payment method

--- src/config/config.js
+++ src/config/config.js
@@ -4,4 +4,4 @@
 
 export const BASE_URL = "https://chainsphere.tech/backend";
 export const BASE_URL_ = "http://127.0.0.1:5000";
-export const BASE_URL1 = "http://localhost:5000/";
+export const BASE_URL1 = "http://127.0.0.1:5000";
